a9b9d1bae164de83b5d2b6f24732b42ff9e19b35
Fernando Brandao <fernando.brandao@adarga.ai>
2019-01-04 08:53:03 +0000

    Linking to the MovieForm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from 'react-router-dom';
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faGhost } from "@fortawesome/free-solid-svg-icons";
 import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
+import MovieForm from "./components/movieForm";
 import "./App.css";
 
 class App extends Component {
@@ -16,6 +15,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
             <Switch>
+            <Route path="/movies/:id" component={MovieForm}></Route>
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" component={Customers}></Route>
             <Route path="/rentals" component={Rentals}></Route>
