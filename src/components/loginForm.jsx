6298558bac6ff68fbad7be40d7684fc398b377d9
Fernando Brandao <fernando.brandao@adarga.ai>
2019-01-08 13:12:42 +0000

    refactoring

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Input from './common/input';
 
 class LoginForm extends Component {
     state = {
@@ -17,33 +18,22 @@ class LoginForm extends Component {
 
     render() {
         const { account } = this.state;
-        return  <div>
+        return  <div >
                     <h1>Login</h1>
                     <form onSubmit={this.handleSubmit}>
-                            <div className="form-group">
-                                <label htmlFor="username">Username</label>
-                                <input
-                                    autoFocus
-                                    value={account.username}
-                                    onChange={this.handleChange}
-                                    id="username"
-                                    name="username"
-                                    type="text"
-                                    className="form-control"
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="password">Password</label>
-                                <input
-                                    value={account.password}
-                                    onChange={this.handleChange}
-                                    name="password"
-                                    id="password"
-                                    type="text"
-                                    className="form-control"
-                                />
-                            </div>
-                    <button className="btn btn-primary">Login</button>
+                        <Input
+                            name="username"
+                            value={account.username}
+                            label="Username"
+                            onChange={this.handleChange}
+                        />
+                        <Input
+                            name="password"
+                            value={account.password}
+                            label="Password"
+                            onChange={this.handleChange}
+                        />
+                        <button className="btn btn-primary">Login</button>
                     </form>
                 </div>;
     }
