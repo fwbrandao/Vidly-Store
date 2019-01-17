a9e4bb000cccef3cadd3160851c9b250b7e6c7dd
Fernando Brandao <fernando.brandao@adarga.ai>
2019-01-17 17:27:27 +0000

    Extracting Helper Rendering Methods

---
+++ src/components/common/form.jsx
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import Input from './input';
+import Joi from 'joi-browser';
+
+class Form extends Component {
+    state = {
+        data: {},
+        errors: {}
+    };
+
+    validate = () => {
+        const options = { abortEarly: false };
+        const { error } = Joi.validate(this.state.data, this.schema, options);
+        if (!error) return null;
+
+        const errors = {};
+        for (let item of error.details) errors[item.path[0]] = item.message;
+        return errors;
+    };
+
+    validateProperty = ({name, value}) => {
+        if (name === 'username'){
+            if(value.trim() === '') return "Username is required.";
+        }
+        if (name === 'password'){
+            if(value.trim() === '') return "Password is required.";
+        }
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const errors = this.validate();
+        console.log(errors);
+        this.setState({ errors: errors || {} });
+        if (errors) return;
+
+        this.doSubmit();
+    };
+
+    handleChange = ({ currentTarget: input }) => {
+
+        const errors =  {...this.errors};
+        const errorMessage = this.validateProperty(input);
+        if (errorMessage) errors[input.name] = errorMessage;
+        else delete errors[input.name];
+
+        const data = { ...this.state.data };
+        data[input.name] = input.value;
+        this.setState({ data, errors });
+    };
+
+    renderButton(label) {
+        return (
+            <button
+                // enable/disable button
+                disabled={this.validate()}
+                className="btn btn-primary">
+                {label}
+            </button>
+        );
+    }
+
+    renderInput(name, label, type = 'text') {
+        const { data, errors } = this.state;
+
+        return (
+            <Input
+                autoFocus
+                type={type}
+                name={name}
+                value={data[name]}
+                label={label}
+                onChange={this.handleChange}
+                error={errors[name]}
+            />
+        );
+    }
+}
+
+export default Form;
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Form from './common/form';
 import Joi from 'joi-browser';
-import Input from './common/input';
 
-class LoginForm extends Component {
+
+class LoginForm extends Form {
     state = {
-        account: { username: '', password: ''},
+        data: { username: '', password: ''},
         errors: {}
     };
 
@@ -13,73 +14,23 @@ class LoginForm extends Component {
         password: Joi.string().required().label('Password')
     }
 
-    validate = () => {
-        const options = { abortEarly: false };
-        const { error } = Joi.validate(this.state.account, this.schema, options);
-        if (!error) return null;
-
-        const errors = {};
-        for (let item of error.details) errors[item.path[0]] = item.message;
-        return errors;
-    };
-
-    handleSubmit = e => {
-        e.preventDefault();
-
-        const errors = this.validate();
-        console.log(errors);
-        this.setState({ errors: errors || {} });
-        if (errors) return;
-
+    doSubmit = () => {
+        // call the server
         console.log("Submitted");
     };
 
-    validateProperty = ({name, value}) => {
-        if (name === 'username'){
-            if(value.trim() === '') return "Username is required.";
-        }
-        if (name === 'password'){
-            if(value.trim() === '') return "Password is required.";
-        }
-    };
-
-    handleChange = ({ currentTarget: input }) => {
-
-        const errors =  {...this.errors};
-        const errorMessage = this.validateProperty(input);
-        if (errorMessage) errors[input.name] = errorMessage;
-        else delete errors[input.name];
-
-        const account = { ...this.state.account };
-        account[input.name] = input.value;
-        this.setState({ account, errors });
-    };
-
-
     render() {
-        const { account, errors } = this.state;
-        return  <div >
+        return  (
+            <div>
                 <h1>Login</h1>
                     <form onSubmit={this.handleSubmit}>
-                        <Input
-                            autoFocus
-                            name="username"
-                            value={account.username}
-                            label="Username"
-                            onChange={this.handleChange}
-                            error={errors.username}
-                        />
-                        <Input
-                            name="password"
-                            value={account.password}
-                            label="Password"
-                            onChange={this.handleChange}
-                            error={errors.password}
-                        />
-                        <button className="btn btn-primary">Login</button>
+                        {this.renderInput('username', 'Username')}
+                        {this.renderInput('password', 'Password', 'password')}
+                        {this.renderButton('Login')}
                     </form>
-                </div>;
-            }
+                </div>
+            );
         }
+    }
 
 export default LoginForm;
