c0d7ec0d6673260b5ab29067a897a4ed95809ec5
Fernando Brandao <fernando.brandao@adarga.ai>
2019-02-06 08:53:23 +0000

    register server call

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Form from './common/form';
 import Joi from 'joi-browser';
-
+import * as userService from '../services/userService';
 
 class RegisterForm extends Form {
     state = {
@@ -15,9 +15,18 @@ class RegisterForm extends Form {
         name: Joi.string().required().label('Name')
     }
 
-    doSubmit = () => {
-        // call the server
-        console.log("Submitted");
+    // call the server
+    doSubmit = async () => {
+        try {
+            await userService.register(this.state.data);
+            console.log("Submitted");
+        } catch (ex) {
+            if (ex.response && ex.response.status === 400) {
+                const errors = { ...this.state.errors };
+                errors.username = ex.response.data;
+                this.setState({ errors });
+            }
+        }
     };
 
     render() {
