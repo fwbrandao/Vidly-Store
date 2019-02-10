5dac990cf75963b259169ed3c7b5b62d5d023539
Fernando Brandao <fwbrandao@gmail.com>
2019-02-10 17:23:37 +0000

    deleted yarn

---
+++ build/precache-manifest.d0c02171dd56753c0c8850d2595f7007.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "910e9665e4c40868e36d",
+    "url": "/static/css/main.4fe85095.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "2b3b97e5ccd16aff25f9",
+    "url": "/static/js/1.2b3b97e5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "910e9665e4c40868e36d",
+    "url": "/static/js/main.910e9665.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "2b3b97e5ccd16aff25f9",
+    "url": "/static/css/1.16412626.chunk.css"
+  },
+  {
+    "revision": "32728a85ea0d95528ba14ed270fefdb3",
+    "url": "/index.html"
+  }
+];
