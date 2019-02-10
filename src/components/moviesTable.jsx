d767f5c0f9070aecab02875653bc62aa566f9ab4
Fernando Brandao <fwbrandao@gmail.com>
2019-02-10 14:36:26 +0000

    hiding delete column

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
+import auth from '../services/authService';
 import Like from "./common/like";
 import Table from "./common/table";
 
@@ -9,28 +10,39 @@ class MoviesTable extends Component {
         { path: 'genre.name', label: 'Genre' },
         { path: 'numberInStock', label: 'Stock' },
         { path: 'dailyRentalRate', label: 'Rate' },
-        { key: 'like',
-        content: movie => (<Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />)},
-        { key: 'delete',
-        content: movie => (
-        <button
-        onClick={() => this.props.onDelete(movie)}
-        className="btn btn-danger btn-sm">
-        Delete
-        </button>
-        )
-    },
-];
+        {
+            key: 'like',
+            content: movie => (<Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />)
+        }
+    ];
+
+    deleteColumn = {
+            key: 'delete',
+            content: movie => (
+                <button
+                    onClick={() => this.props.onDelete(movie)}
+                    className="btn btn-danger btn-sm">
+                    Delete
+                </button>
+            )
+        }
+
+    constructor() {
+        super();
+        const user = auth.getCurrentUser();
+        if (user && user.isAdmin)
+            this.columns.push(this.deleteColumn);
+    }
 
     render() {
-        const { movies, onSort, sortColumn} = this.props;
+        const { movies, onSort, sortColumn } = this.props;
 
         return (
             <Table
-            columns={this.columns}
-            data={movies}
-            sortColumn={sortColumn}
-            onSort={onSort}
+                columns={this.columns}
+                data={movies}
+                sortColumn={sortColumn}
+                onSort={onSort}
             />
         );
     }
