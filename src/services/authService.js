2f604d69c111241828aa008d08a6eb026a2e6653
Fernando Brandao <fwbrandao@gmail.com>
2019-02-17 13:56:40 +0000

    fix to authService

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,12 +1,13 @@
+import jwtDecode from "jwt-decode";
 import http from "./httpService";
-import jwtDecode from 'jwt-decode';
+import { apiUrl } from "../config.json";
 
-const apiEndpoint = "/auth";
-const tokenKey = 'token';
+const apiEndpoint = apiUrl + "/auth";
+const tokenKey = "token";
 
 http.setJwt(getJwt());
 
-export async function  login(email, password) {
+export async function login(email, password) {
     const { data: jwt } = await http.post(apiEndpoint, { email, password });
     // saves JWT in the browser localStorage
     localStorage.setItem(tokenKey, jwt);
@@ -24,9 +25,9 @@ export function getCurrentUser() {
     try {
         const jwt = localStorage.getItem(tokenKey);
         return jwtDecode(jwt);
-      } catch (ex) {
-          return null;
-      }
+    } catch (ex) {
+        return null;
+    }
 }
 
 export function getJwt() {
@@ -35,8 +36,8 @@ export function getJwt() {
 
 export default {
     login,
+    loginWithJwt,
     logout,
     getCurrentUser,
-    loginWithJwt,
     getJwt
-}
+};
